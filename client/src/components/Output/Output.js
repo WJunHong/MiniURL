30c75a7eee8aaf53e637e55eb7f6c479b3a6b39a
Wang Jun Hong <e0544005@u.nus.edu>
2023-02-14 13:11:48 +0800

    Add responsiveness to containers

--- client/src/components/Output/Output.js
+++ client/src/components/Output/Output.js
@@ -7,8 +7,8 @@ import Paper from "@mui/material/Paper";
 const Output = ({ miniUrl }) => {
   return (
     <div className={styles.container}>
-      <Paper elevation={3} className={styles.inputContainer}>
-        <div>MiniURL:</div>
+      <Paper elevation={3} className={styles.outputContainer}>
+        <div>MiniURL</div>
         <a href={`${miniUrl}`} target="_blank">
           {miniUrl}
         </a>
